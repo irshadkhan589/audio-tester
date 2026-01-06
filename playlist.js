62478ed379b680167884069a0cb9ec2c2f9a98fc
Irshad Ahmed Khan <irshadkhan589@gmail.com>
2026-01-06 05:58:09 +0530

    Added two new songs

--- playlist.js
+++ playlist.js
@@ -3,4 +3,6 @@ const playlist = [
   { title: "Against All Odds AP Dhillon", file: "music/AgainstAllOdds.mp3" },
   { title: "Gigachad Theme", file: "music/gigachad.mp3" },
   { title: "Metamorphosis Interworld", file: "music/metamorphosis.mp3" },
+  { title: "Sahara Hensonn", file: "music/saharahensonn.mp3" },
+  { title: "Crystals Isolate", file: "music/crystalsIsolate.mp3" },
 ];
